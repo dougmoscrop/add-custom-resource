c506827b056d36c7ad39576de102e5be882d779c
Doug Moscrop <doug.moscrop@gmail.com>
2017-08-06 11:25:15 -0400

    check that source code for custom resource has module.exports.handler = (easy mistake to make that causes CF deploy to hang)

--- test/lib/get-source-code-lines.js
+++ test/lib/get-source-code-lines.js
@@ -5,8 +5,12 @@ const sinon = require('sinon');
 const proxyquire = require('proxyquire');
 
 test('reads, splits and escapes source', t => {
-  const escapeJsonNode = sinon.mock().exactly(2).onCall(0).returns('foo').onCall(1).returns('bar');
-  const readFileSync = sinon.mock().returns(Buffer.from(`asdf\r\nqwerty`));
+  const escapeJsonNode = sinon.mock().exactly(3)
+    .onCall(0).returns('foo')
+    .onCall(1).returns('bar')
+    .onCall(2).returns('baz');
+
+  const readFileSync = sinon.mock().returns(Buffer.from(`module.exports.handler = function() {\r\nreturn;\r\n}`));
 
   const getSourceCodeLines = proxyquire('../../lib/get-source-code-lines', {
     'escape-json-node': escapeJsonNode,
@@ -22,4 +26,21 @@ test('reads, splits and escapes source', t => {
 
   t.true(sourceCodeLines[0] === 'foo');
   t.true(sourceCodeLines[1] === 'bar');
+  t.true(sourceCodeLines[2] === 'baz');
+});
+
+test('throws if it lacks module.exports.handler = ', t => {
+  const escapeJsonNode = sinon.mock().exactly(2).onCall(0).returns('foo').onCall(1).returns('bar');
+  const readFileSync = sinon.mock().returns(Buffer.from(`asdf\r\nqwerty`));
+
+  const getSourceCodeLines = proxyquire('../../lib/get-source-code-lines', {
+    'escape-json-node': escapeJsonNode,
+    'fs': {
+      readFileSync
+    }
+  });
+
+  const err = t.throws(() => getSourceCodeLines());
+
+  t.true(err.message === `missing 'module.exports.handler =' in source code`);
 });
